bbfdea7bd717c47038385e7d1f9feefcb9e893b6
adq_dvb@lidskialf.net <adq_dvb@lidskialf.net@1deccd60-327e-11de-82fb-7f665ef830f7>
2009-04-27 21:51:38 +0000

    Add timelapse script

---
+++ Timelapse.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import traceback
+import time
+from ptp.PtpUsbTransport import PtpUsbTransport
+from ptp.PtpSession import PtpSession, PtpException
+from ptp import PtpValues
+
+if len(sys.argv) != 2:
+    print >>sys.stderr, "Syntax: TimelapseCapture <interval in seconds>"
+    sys.exit(1)
+delay = int(sys.argv[1])
+
+ptpTransport = PtpUsbTransport(PtpUsbTransport.findptps()[0])
+ptpSession = PtpSession(ptpTransport)
+
+vendorId = PtpValues.Vendors.STANDARD
+try:
+    ptpSession.OpenSession()
+    deviceInfo = ptpSession.GetDeviceInfo()
+    vendorId = deviceInfo.VendorExtensionID
+
+    while True:
+        ptpSession.InitiateCapture(objectFormatId=PtpValues.StandardObjectFormats.EXIF_JPEG)
+        while True:
+            evt = ptpSession.CheckForEvent(None)
+            if evt.eventcode == PtpValues.StandardEvents.OBJECT_ADDED:
+                break
+
+        time.sleep(delay * 1000)
+
+except PtpException, e:
+    print "PTP Exception: %s" % PtpValues.ResponseNameById(e.responsecode, vendorId)
+except Exception, e:
+    print "An exception occurred: %s" % e
+    traceback.print_exc()
+
+del ptpSession
+del ptpTransport
